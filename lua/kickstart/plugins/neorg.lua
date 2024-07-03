1f1cfe5bb765a61584ea4a6eeb623bfd19f1e4c3
Dave Verstrate <daverstrate@gmail.com>
2024-07-03 17:11:05 +0200

    adjust neorg config

--- lua/kickstart/plugins/neorg.lua
+++ lua/kickstart/plugins/neorg.lua
@@ -1,4 +1,27 @@
 return {
   'nvim-neorg/neorg',
-  opts = {},
+  config = function()
+    require('neorg').setup {
+      load = {
+        ['core.defaults'] = {},
+        ['core.completion'] = {
+          config = {
+            engine = 'nvim-cmp',
+            name = '[Neorg]',
+          },
+        },
+        ['core.concealer'] = {},
+        ['core.dirman'] = {
+          config = {
+            workspaces = {
+              notes = '~/workspace/neorg-notes',
+            },
+            default_workspace = 'notes',
+          },
+        },
+      },
+    }
+    vim.wo.foldlevel = 99
+    vim.wo.conceallevel = 2
+  end,
 }
