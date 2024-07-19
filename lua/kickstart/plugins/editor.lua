341c506f1429f4a8f0dd7d492683f4a9a8040a6e
Dave Verstrate <daverstrate@gmail.com>
2024-07-19 12:43:46 +0200

    add zenmode

--- lua/kickstart/plugins/editor.lua
+++ lua/kickstart/plugins/editor.lua
@@ -18,4 +18,16 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {},
   },
+  {
+    'folke/zen-mode.nvim',
+    opts = {
+      plugins = {
+        kitty = { enabled = true },
+      },
+    },
+  },
+  {
+    'folke/twilight.nvim',
+    opts = {},
+  },
 }
