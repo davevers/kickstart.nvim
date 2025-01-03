9f23a2091c84433d65dfaf74a79bc8540d671feb
Dave Verstrate <daverstrate@gmail.com>
2025-01-03 18:34:54 +0100

    adjust conform options

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,5 +1,5 @@
 return {
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  { 'tpope/vim-sleuth' }, -- Detect tabstop and shiftwidth automatically
   { -- Autoformat
     'stevearc/conform.nvim',
     lazy = false,
@@ -27,7 +27,8 @@ return {
         json = { 'fixjson' },
         jsonc = { 'fixjson' },
         css = { 'stylelint' },
-        markdown = { { 'prettierd', 'prettier' } },
+        markdown = { 'prettierd' },
+        go = { 'gofmt' },
       },
     },
   },
