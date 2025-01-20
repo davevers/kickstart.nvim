d8abe59b3a58c52d6c30fc2f2d602ea888fdd649
Dave <daverstrate@gmail.com>
2025-01-20 08:31:30 +0100

    make colorscheme transparent

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,8 +2,12 @@ return {
   {
     'catppuccin/nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      vim.cmd.colorscheme 'catppuccin-mocha'
+    opts = {
+      transparent_background = true,
+    },
+    config = function(_, opts)
+      require('catppuccin').setup(opts)
+      vim.cmd.colorscheme 'catppuccin-macchiato'
     end,
   },
 }
