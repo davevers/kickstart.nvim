b1569f383c4396d316ede1cfd6ea432648cdb9cf
Dave Verstrate <daverstrate@gmail.com>
2025-01-24 09:19:11 +0100

    add chezmoi.vim

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -45,4 +45,12 @@ return {
   },
   -- Color preview for hex codes
   { 'norcalli/nvim-colorizer.lua' },
+  {
+    -- highlighting for chezmoi files template files
+    'alker0/chezmoi.vim',
+    init = function()
+      vim.g['chezmoi#use_tmp_buffer'] = 1
+      vim.g['chezmoi#source_dir_path'] = os.getenv 'HOME' .. '/.local/share/chezmoi'
+    end,
+  },
 }
