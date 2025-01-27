22be931c313d1534e1afcd05d02499403bad0590
Dave Verstrate <daverstrate@gmail.com>
2025-01-27 14:08:05 +0100

    fix typo

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -4,7 +4,7 @@ return {
     dependencies = { 'echasnovski/mini.icons' },
     opts = {
       options = {
-        theme = 'catppuccin-machiato',
+        theme = 'catppuccin-macchiato',
         section_separators = '',
         component_separators = '',
       },
